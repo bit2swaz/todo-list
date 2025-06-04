3e03c2f7edec05c763e61f2c87f29ba2de8b47b8
Aditya <bit2swaz@gmail.com>
2025-06-04 10:34:16 +0000

    Setup GitHub Pages deployment

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     filename: 'main.[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: '/todo-list/'
   },
   module: {
     rules: [
